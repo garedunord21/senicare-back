2d73a4cdecb48c019da8d7f2b3b6c462fa0e1f82
garedunord21 <garedunord21@gmail.com>
2024-09-10 12:51:00 +0900

    2024.09.10 12:50

--- senicare.sql
+++ senicare.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `senicare`.`nurses` (
   `password` VARCHAR(255) NOT NULL COMMENT '암호화된 비밀번호',
   `name` VARCHAR(15) NOT NULL COMMENT '요양사 이름',
   `tel_number` VARCHAR(11) NOT NULL COMMENT '요양사 전화번호',
-  `join_path` VARCHAR(5) NOT NULL COMMENT '가입 경로 (HOME: 기본, KAKAO: 카카오, ',
+  `join_path` VARCHAR(5) NOT NULL COMMENT '가입 경로 ( HOME : 기본, KAKAO : 카카오, NAVER : 네이버 )',
   `sns_id` VARCHAR(255) NULL COMMENT 'OAuth2 아이디',
   PRIMARY KEY (`user_id`),
   UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE,
@@ -55,14 +55,15 @@ COMMENT = '요양사 테이블';
 -- Table `senicare`.`customers`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `senicare`.`customers` (
-  `custom_number` INT NOT NULL,
+  `custom_number` INT NOT NULL AUTO_INCREMENT COMMENT '고객 번호',
   `profile_image` TEXT NOT NULL COMMENT '고객 프로필 사진',
   `name` VARCHAR(15) NOT NULL COMMENT '고객 이름',
-  `birth` VARCHAR(6) NOT NULL,
+  `birth` VARCHAR(6) NOT NULL COMMENT '고객 생년월일 (YYMMDD)',
   `charger` VARCHAR(20) NOT NULL COMMENT '담당 요양사',
-  `address` TEXT NOT NULL,
-  `location` VARCHAR(50) NOT NULL COMMENT '지역(시, 도)',
+  `address` TEXT NOT NULL COMMENT '주소',
+  `location` VARCHAR(60) NOT NULL COMMENT '지역 (시, 도)',
   PRIMARY KEY (`custom_number`),
+  UNIQUE INDEX `custom_number_UNIQUE` (`custom_number` ASC) VISIBLE,
   INDEX `customer_charger_idx` (`charger` ASC) VISIBLE,
   CONSTRAINT `customer_charger`
     FOREIGN KEY (`charger`)
@@ -78,9 +79,9 @@ COMMENT = '고객 테이블';
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `senicare`.`tools` (
   `tool_number` INT NOT NULL AUTO_INCREMENT COMMENT '용품 번호',
-  `name` VARCHAR(255) NOT NULL,
-  `purpose` TEXT NOT NULL,
-  `count` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(255) NOT NULL COMMENT '용품 이름',
+  `purpose` TEXT NOT NULL COMMENT '용품 용도',
+  `count` INT NOT NULL COMMENT '용품 잔여 수량',
   PRIMARY KEY (`tool_number`),
   UNIQUE INDEX `tool_number_UNIQUE` (`tool_number` ASC) VISIBLE)
 ENGINE = InnoDB
@@ -92,14 +93,14 @@ COMMENT = '용품 테이블';
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `senicare`.`care_records` (
   `record_number` INT NOT NULL AUTO_INCREMENT COMMENT '관리 기록 번호',
-  `record_date` DATE NOT NULL,
-  `contents` TEXT NOT NULL,
+  `record_date` DATE NOT NULL COMMENT '관리 기록 날짜',
+  `contents` TEXT NOT NULL COMMENT '관리 내용',
   `used_tool` INT NULL COMMENT '관리에 사용한 용품',
   `count` INT NULL COMMENT '관리시 사용한 용품 개수',
   `charger` VARCHAR(20) NOT NULL COMMENT '관리 담당 요양사',
   `custom_number` INT NOT NULL COMMENT '관리 대상 고객',
   PRIMARY KEY (`record_number`),
-  UNIQUE INDEX `idcare_records_UNIQUE` (`record_number` ASC) VISIBLE,
+  UNIQUE INDEX `record_number_UNIQUE` (`record_number` ASC) VISIBLE,
   INDEX `used_tools_idx` (`used_tool` ASC) VISIBLE,
   INDEX `record_charger_idx` (`charger` ASC) VISIBLE,
   INDEX `cared_customer_idx` (`custom_number` ASC) VISIBLE,
@@ -119,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `senicare`.`care_records` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-COMMENT = '관리 기록';
+COMMENT = '관리 기록 테이블';
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
