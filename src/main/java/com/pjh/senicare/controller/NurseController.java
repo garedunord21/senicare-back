992ea8d83883d08ae5ac21ce669aea006582d5ca
garedunord21 <garedunord21@gmail.com>
2024-09-19 12:49:10 +0900

    2024.09.19 12:50

--- src/main/java/com/pjh/senicare/controller/NurseController.java
+++ src/main/java/com/pjh/senicare/controller/NurseController.java
@@ -1,8 +1,12 @@
 package com.pjh.senicare.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pjh.senicare.dto.response.nurse.GetSignInResponseDto;
 import com.pjh.senicare.service.NurseService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,5 +17,13 @@ import lombok.RequiredArgsConstructor;
 public class NurseController {
     
     private final NurseService nurseService;
+
+    @GetMapping("/sign-in")
+    public ResponseEntity<? super GetSignInResponseDto> getSignIn(
+        @AuthenticationPrincipal String userId
+    ) {
+        ResponseEntity<? super GetSignInResponseDto> response = nurseService.getSignIn(userId);
+        return userId;
+    }
 
 }
--- src/main/java/com/pjh/senicare/service/implement/NurseServiceImplement.java
+++ src/main/java/com/pjh/senicare/service/implement/NurseServiceImplement.java
@@ -1,10 +1,39 @@
 package com.pjh.senicare.service.implement;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.pjh.senicare.dto.response.ResponseDto;
+import com.pjh.senicare.dto.response.nurse.GetSignInResponseDto;
+import com.pjh.senicare.entity.NurseEntity;
+import com.pjh.senicare.repository.NurseRepository;
 import com.pjh.senicare.service.NurseService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class NurseServiceImplement implements NurseService {
+
+    private final NurseRepository nurseRepository;
+
+    @Override
+    public ResponseEntity<? super GetSignInResponseDto> getSignIn(String userId) {
+        
+        NurseEntity nurseEntity = null;
+        
+        try {
+
+            nurseEntity = nurseRepository.findByUserId(userId);
+            if (nurseEntity == null) return ResponseDto.noExistUserId();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetSignInResponseDto.success(nurseEntity);
+        
+    }
     
 }
